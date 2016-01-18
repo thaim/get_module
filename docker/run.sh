d4b138550bdd32544cc0a1a927b6ca78ece1b064
thaim <thaim24@gmail.com>
2016-01-18 23:52:22 +0900

    move files for docker into docker dir

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,5 @@
 
 docker run --rm \
        -v `pwd`/modules:/modules \
-       -v `pwd`/modulelist.yml:/etc/modulelist.yml \
+       -v `pwd`/../modulelist.yml:/etc/modulelist.yml \
        thaim/get_module
